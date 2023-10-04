f6dbd6fcbb1059c0a91c646e9ec8d71e06c58f34
Igor Gunin <gunin-igor75@yandex.ru>
2023-10-04 17:01:43 +0600

    initialization project

---
+++ app/src/main/java/com/github/gunin_igor75/example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.github.gunin_igor75.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
