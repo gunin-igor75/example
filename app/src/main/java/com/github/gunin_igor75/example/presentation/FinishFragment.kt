ae3a7055f55e84513808e6b959e90a32a70a77de
Igor Gunin <gunin-igor75@yandex.ru>
2023-10-12 14:17:51 +0600

    fix 1.0

--- app/src/main/java/com/github/gunin_igor75/example/presentation/FinishFragment.kt
+++ app/src/main/java/com/github/gunin_igor75/example/presentation/FinishFragment.kt
@@ -5,8 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
@@ -14,11 +12,6 @@ import com.github.gunin_igor75.example.databinding.FragmentFinishBinding
 
 class FinishFragment : Fragment() {
     private var _binding: FragmentFinishBinding? = null
-    private lateinit var ivLogoGameOver: ImageView
-    private lateinit var tvEnoughAnswers: TextView
-    private lateinit var tvScore: TextView
-    private lateinit var tvEnoughPercent: TextView
-    private lateinit var tvPercent: TextView
     private lateinit var btAgainBegin: Button
 
     private val args by navArgs<FinishFragmentArgs>()
@@ -50,11 +43,6 @@ class FinishFragment : Fragment() {
     }
 
     private fun initView() {
-        ivLogoGameOver = binding.ivLogoGameOver
-        tvEnoughAnswers = binding.tvEnoughAnswers
-        tvScore = binding.tvScore
-        tvEnoughPercent = binding.tvEnoughPercent
-        tvPercent = binding.tvPercent
         btAgainBegin = binding.btAgainBegin
     }
 
